c0448b8e6a93d4f8ce61f483830e28022917e82a
Momme <momm@pml.ac.uk>
2015-11-27 17:03:36 +0000

    Added configuration for pure physics run without tracers.

---
+++ NEMOGCM/CONFIG/AMM7_MYTRC/EXP00/rebuild-nemo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+module load mpi
+
+NEMO_BUILD_DIR=$HOME/git/NEMO-shelf/NEMOGCM/CONFIG
+RUNDIR=/data/euryale4/scratch/momm/NEMO-FABM-AMM7-3.6/AMM7-MYSRC
+export XIOS_HOME=$HOME/git/XIOS-1.0-svn
+export FABM_HOME=$HOME/local/fabm/nemo
+
+ARCH=GCC_PMPC
+#ARCH=GCC_PMPC_DEBUG
+
+cd $NEMO_BUILD_DIR
+echo "Building NEMO-FABM..."
+
+./makenemo -m $ARCH -n AMM7_TEST && rsync -a $NEMO_BUILD_DIR/AMM7_TEST/BLD/bin/nemo.exe $RUNDIR/
