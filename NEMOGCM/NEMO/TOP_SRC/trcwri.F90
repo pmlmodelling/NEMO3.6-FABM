be9ff541ad4f10e73648603de3b8560384b16457
Momme Butenschön <momm@pml.ac.uk>
2015-12-15 23:02:51 +0000

    Patch fixes.

--- NEMOGCM/NEMO/TOP_SRC/trcwri.F90
+++ NEMOGCM/NEMO/TOP_SRC/trcwri.F90
@@ -20,6 +20,9 @@ MODULE trcwri
    USE trcwri_cfc
    USE trcwri_c14b
    USE trcwri_my_trc
+   ! +++>>> FABM
+   USE trcwri_fabm
+   ! FABM <<<+++
 
    IMPLICIT NONE
    PRIVATE
@@ -72,7 +75,7 @@ CONTAINS
       IF( lk_my_trc  )   CALL trc_wri_my_trc     ! MY_TRC  tracers
 #endif
       ! +++>>>FABM
-      IF( lk_fabm    )   CALL trc_sms_fabm ( kt )      ! FABM tracers
+      IF( lk_fabm    )   CALL trc_wri_fabm      ! FABM tracers
       ! FABM <<<+++
       !
       IF( nn_timing == 1 )  CALL timing_stop('trc_wri')
