a2b77aa0eb71bc854f82b86604705305bdb6cc45
Momme Butenschoen <momm@pml.ac.uk>
2016-02-08 14:57:33 +0000

    Updated PAPA configuration of FABM-ERSEM.

--- NEMOGCM/CONFIG/C1D_PAPA_FABM_ERSEM/EXP00/build_nemo.sh
+++ NEMOGCM/CONFIG/C1D_PAPA_FABM_ERSEM/EXP00/build_nemo.sh
@@ -2,13 +2,14 @@
 
 NEMO_BUILD_DIR=$HOME/git/NEMO-shelf/NEMOGCM/CONFIG
 RUNDIR=/data/C1D_PAPA
-
 export XIOS_HOME=$HOME/local/xios-gnu
+export FABM_HOME=$HOME/local/fabm/nemo
+
+ARCH=GCC_UBUNTU
+
 cd $NEMO_BUILD_DIR
 echo "Building NEMO..."
-./makenemo -m "GCC_UBUNTU" -n C1D_PAPA_TEST clean_config
-#./makenemo -m "GCC_UBUNTU_DEBUG" -r C1D_PAPA_FABM -n C1D_PAPA_TEST && rsync -a $NEMO_BUILD_DIR/ORCA2_LIM_FABM/BLD/bin/nemo.exe $RUNDIR/
-./makenemo -m "GCC_UBUNTU" -r C1D_PAPA_FABM_ERSEM -n C1D_PAPA_TEST && rsync -a $NEMO_BUILD_DIR/C1D_PAPA_TEST/BLD/bin/nemo.exe $RUNDIR/
-#./makenemo -m "GCC_UBUNTU" -n C1D_PAPA_TEST && rsync -a $NEMO_BUILD_DIR/C1D_PAPA_TEST/BLD/bin/nemo.exe $RUNDIR/
-echo "Done."
 
+./makenemo -m $ARCH -n C1D_PAPA_TEST clean_config
+./makenemo -m $ARCH -r C1D_PAPA_FABM_ERSEM -n C1D_PAPA_TEST && rsync -a $NEMO_BUILD_DIR/C1D_PAPA_TEST/BLD/bin/nemo.exe $RUNDIR/
+echo "Done."
