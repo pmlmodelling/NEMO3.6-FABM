6631a3500fadb933cb46ae0dad7d04f9e5c813ac
Momme Butenschoen <momm@pml.ac.uk>
2016-03-15 06:47:50 +0000

    Updated build scripts for ORCA2 FABM-NEMO case.

--- NEMOGCM/CONFIG/ORCA2_LIM_FABM_ERSEM/EXP00/build-nemo.sh
+++ NEMOGCM/CONFIG/ORCA2_LIM_FABM_ERSEM/EXP00/build-nemo.sh
@@ -8,9 +8,9 @@ export FABM_HOME=$HOME/local/fabm/nemo
 ARCH=GCC_UBUNTU
 
 cd $NEMO_BUILD_DIR
-echo "Cleaning old build..."
+echo "Cleaning old build ..."
 rm -f $RUNDIR/nemo.exe
 ./makenemo -m $ARCH -n ORCA2_LIM_FABM_BLD_SCRATCH clean_config
 
 echo "Building NEMO-FABM..."
-./makenemo -m $ARCH -r ORCA2_LIM_FABM_ERSEM -n ORCA2_LIM_FABM_BLD_SCRATCH > $RUNDIR/compile.log && rsync -a $NEMO_BUILD_DIR/ORCA2_LIM_FABM_BLD_SCRATCH/BLD/bin/nemo.exe $RUNDIR/ && echo "Done."
+./makenemo -m $ARCH -r ORCA2_LIM_FABM_ERSEM -n ORCA2_LIM_FABM_BLD_SCRATCH 2>&1 | tee compile.log && rsync -a $NEMO_BUILD_DIR/ORCA2_LIM_FABM_BLD_SCRATCH/BLD/bin/nemo.exe $RUNDIR/ && echo "Done."
